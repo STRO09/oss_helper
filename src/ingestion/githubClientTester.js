58b5070efd554ea20ba67eb252beb8c158f70b50
SidoIndeedo <sidharths1001@gmail.com>
2026-03-17 15:20:22 +0530

    git client tester updated

--- src/ingestion/githubClientTester.js
+++ src/ingestion/githubClientTester.js
@@ -2,12 +2,14 @@ const { ingestIssuse } = require("./ingestIssues");
 const path = require("path");
 const dotenv = require("dotenv");
 const { connectDB } = require("../config/db");
+const { computeSignalForAllIssues } = require("../signals/computeSignal");
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 async function run() {
   await connectDB();
   await ingestIssuse();
+  await computeSignalForAllIssues();
   process.exit(0);
 }
 
