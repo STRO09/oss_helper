5c008f1a67c3245938713487880db96ce858abc5
STRO09 <sagarjanjoted123@gmail.com>
2026-03-22 13:27:00 +0530

    [updated]::: updated indexjs as per src/ingestion/githubclienttster to compute signals

--- src/ingestion/index.js
+++ src/ingestion/index.js
@@ -1,17 +1,35 @@
-const { ingestIssuse } = require("./ingestIssues");
 const path = require("path");
 const dotenv = require("dotenv");
+
 const { connectDB } = require("../config/db");
+
+const { ingestIssue } = require("./ingestIssues");
+const { computeSignalForAllIssues } = require("../signals/computeSignal");
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-async function runIngestion() {
+async function runIngestion(totalPagesToFetch = 1) {
+  const rateLimitTimeout = 2000;
   try {
-
     await connectDB();
     console.log("Connected to database...");
-    await ingestIssuse();
+
+    for (let i = 1; i <= totalPagesToFetch; i++) {
+      console.log(`Fetching Page ${i}...`);
+      await ingestIssue(i);
+
+      if (i < totalPagesToFetch) {
+        console.log("Waiting for rate limit safety...");
+        await new Promise((resolve) => setTimeout(resolve, rateLimitTimeout));
+      }
+    }
+
     console.log("Ingestion completed successfully.");
+
+    console.log("Starting Signal computation for all the ingested issues....");
+    await computeSignalForAllIssues();
+    console.log("Signal computation completed successfully.");
+
   } catch (error) {
     console.error("Error during ingestion:", error);
   } finally {
@@ -19,8 +37,10 @@ async function runIngestion() {
   }
 }
 
+const totalPagesToFetch = 15;
+
 if (require.main === module) {
-  runIngestion();
+  runIngestion(totalPagesToFetch);
 }
 
-module.exports = { runIngestion, ingestIssuse };
+module.exports = { runIngestion };
